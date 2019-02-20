1af2a57f4eab580e84ef59668e4b9352dc2fc64a
dinopuguh <puguhmunung16@gmail.com>
2019-02-20 11:57:01 +0700

    add routes

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const roles = require('./routes/api/roles');
+const workingOrders = require('./routes/api/workingOrders');
+const types = require('./routes/api/types');
+const priorities = require('./routes/api/priorities');
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello"));
+require('dotenv').config();
+const db = process.env.MONGO_URI;
+
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello'));
+
+app.use('/api/users', users);
+app.use('/api/roles', roles);
+app.use('/api/working-orders', workingOrders);
+app.use('/api/types', types);
+app.use('/api/priorities', priorities);
 
 const port = process.env.PORT || 5000;
 
